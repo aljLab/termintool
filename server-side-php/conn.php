1d0f3cf9a7eea4b6a4a54c5735bdd4795af33d29
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-10-03 00:07:22 +0200

    UTF-8-Meta Tag

--- server-side-php/conn.php
+++ server-side-php/conn.php
@@ -1,11 +1,13 @@
 <?php
-    /*$dns ="mysql:host=localhost;dbname=login";
+    $dns ="mysql:host=localhost;dbname=termintool";
     $user = $_SERVER["dbuser"];
     $password =$_SERVER["dbuserpassword"];
     try{
         $conn = new PDO($dns, $user, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM termine";
+        $conn->query(sql);
     }catch(PDOException $e){
         echo "Connection failed: ".$e->getMessage();
-    }*/
+    }
 ?>
