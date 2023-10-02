69172b1ce83dde156d45f4a1c4a43b611b387e14
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-10-03 00:42:33 +0200

    nachtrag

--- server-side-php/login.php
+++ server-side-php/login.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    $n = $_SERVER["admin"];//env Variable
-    $p = $_SERVER["adminpassword"];
+    $n = $_SERVER['admin'];//env Variable
+    $p = $_SERVER['adminpassword'];
     $name = "";
     $ErrorMessage="";
     if($_SERVER["REQUEST_METHOD"]=="POST"){
