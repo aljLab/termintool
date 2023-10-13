379f36520020aa4fbd33c2fea616b5d18276391e
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-10-14 00:01:02 +0200

    Nachtrag

--- server-side-php/settingTermine.php
+++ server-side-php/settingTermine.php
@@ -1,23 +1,22 @@
 <?php
-    $dns ="mysql:host=localhost;dbname=termintool";
-    $user = $_SERVER["dbuser"];
-    $password =$_SERVER["dbuserpassword"];
-    $t = file_get_contents("php://input");//reading POST-Request body from Fetch-Request
-    
-  // Assuming the data is sent as JSON, decode it
-    
-    try {
-        $conn = new PDO($dns, $user, $password);
-        // Insert data into MySQL table (modify the query as needed)
-        $sql = "INSERT INTO termine (termin) VALUES (?)";
-        $stmt = $conn->prepare($sql);
-  
-        if ($stmt) {
-            $stmt->execute($t);
-            echo "Data inserted successfully. $t";
-        } else {
-            echo "Error: " . $conn->errorInfo()[2];
-        }
-    } catch (PDOException $e) {
-      echo "Error: " . $e->getMessage();
-  }
+$dns = "mysql:host=localhost;dbname=termintool";
+$user = $_SERVER["dbuser"];
+$password = $_SERVER["dbuserpassword"];
+$t = file_get_contents("php://input"); // Reading POST-Request body from Fetch-Request
+
+try {
+    $conn = new PDO($dns, $user, $password);
+    $sql = "INSERT INTO termine (termin) VALUES (?)";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bindParam(1, $t, PDO::PARAM_STR);
+        $stmt->execute();
+        echo "Data inserted successfully. $t";
+    } else {
+        echo "Error: " . $conn->errorInfo()[2];
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
