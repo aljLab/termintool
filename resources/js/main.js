0f16822257851b73ff0ba141818d22f0bf2b3528
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-10-20 00:29:27 +0200

    Nachtrag

--- resources/js/main.js
+++ resources/js/main.js
@@ -37,13 +37,13 @@ function Termin(h, m, date, leistung, dauer, kunde){
     this.leistung = leistung;//
     this.dauer = dauer;
     this.kunde = kunde;
-    this.getTimeslot= function(){//to be called from retrieved termin-objects (because dates cant be recovered from db(JSON))
-        dateArr = this.date.split(".");
-        let startDate= new Date(dateArr[2], dateArr[1], dateArr[0], this.hourValue, this.minuteValue);
-        let endDate=new Date(startDate.getTime()+ 1000*60*15*this.dauer);
-        return new Timeslot(this.date, startDate, endDate);
-    };
 }
+Termin.prototype.getTimeslot=function(){//to be called from retrieved termin-objects (because dates cant be recovered from db(JSON))
+    dateArr = this.date.split(".");
+    let startDate= new Date(dateArr[2], dateArr[1], dateArr[0], this.hourValue, this.minuteValue);
+    let endDate=new Date(startDate.getTime()+ 1000*60*15*this.dauer);
+    return new Timeslot(this.date, startDate, endDate);
+};
 function Leistung(name, dauer, preis){
     this.name = name;
     this.dauer = dauer;
