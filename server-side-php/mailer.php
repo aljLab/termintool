1a6daf7f41fe2fb425264ccd2f75d0d367b9bced
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-11-01 23:00:37 +0100

    MailDefault

--- server-side-php/mailer.php
+++ server-side-php/mailer.php
@@ -10,18 +10,13 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     //read POST-Request Body -> Terminobjekt
-    $objectArray = json_decode(file_get_contents("php://input"), true);
+    $objectArray = json_decode(file_get_contents("php://input"), true);//json_decode('{"hourValue":"09","minuteValue":"30","date":"12.10.2023","leistung":{"name":"Anamnese","dauer":"4","preis":"60"},"kunde":{"anrede":"Dr.","vorname":"Aljoscha","nachname":"Lustig","mail":"aljoschalabonte@rocketmail.com","phone":"017623559949"}}');
 
     $lei = $objectArray->leistung;
     $kun = $objectArray->kunde;
     $nameString = "$kun->anrede $kun->nachname";
     $tString = "$objectArray->date, $objectArray->hourValue : $objectArray->minuteValue Uhr";
     
-    var_dump($objectArray);
-    var_dump($lei);
-    var_dump($kun);
-    var_dump($nameString);
-
     $mail = new PHPMailer(true);
 
     try {
@@ -34,22 +29,23 @@
         $mail->Port = 465;
 
         // Set the From address
-        $mail->setFrom('naturheilpraxis.labonte@gmail.com', 'Martina La Bonte');
+        $mail->setFrom('naturheilpraxis.labonte@gmail.com', 'Martina La Bonté');
 
         // Add a recipient
         $mail->addAddress($kun->mail, $kun->anrede." ".$kun->vorname." ".$kun->nachname);
 
         // Content
         $mail->isHTML(true);
+        $mail->CharSet = "UTF-8";
         $mail->Subject = "Terminbestätigung $tString";
-        $mail->Body = "Terminbestätigung\n
+        $mail->Body = nl2br("Terminbestätigung\n
                 Sehr geehrte/r $nameString, \n
                 Hiermit bestätige ich den gebuchten Termin am $tString.
                 Gebucht wurde Leistung: $lei->name ($lei->preis €).
 
                 Bitte antworten Sie nicht auf diese Mail. \n
                 Stornierung und Fragen bitte an skymove@posteo.de.
-            ";
+            ");
 
         // Send the email
         $mail->send();
