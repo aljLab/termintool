00217b52cd9934330055ef90af59abe7ebd0eaca
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-11-19 14:53:46 +0100

    Korrektur Syntax

--- resources/js/main.js
+++ resources/js/main.js
@@ -66,10 +66,10 @@ function Kunde(anrede, vorname, nachname, mail, phone){
 function FerienZeit(d1, d2){
     this.d1= d1;
     this.d2= d2;
-    this.date1=new Date(Number(d1.split(".")[2]), Number(d1.split(".")[1])-1, Number(d1.split("."[0])));
-    this.date2=new Date(Number(d2.split(".")[2]), Number(d2.split(".")[1])-1, Number(d2.split("."[0])));
+    this.date1=new Date(d1.split(".")[2], (d1.split(".")[1])-1,d1.split("."[0]));
+    this.date2=new Date(d2.split(".")[2], (d2.split(".")[1])-1, d2.split("."[0]));
     this.checkIfWithin=(datestr)=>{
-        let compDate= new Date(Number(datestr.split(".")[2]), Number(datestr.split(".")[1])-1, Number(date.split("."[0])));
+        let compDate= new Date(datestr.split(".")[2], datestr.split(".")[1]-1, date.split("."[0]));
         return (this.date1 <= compDate)&&( compDate<=this.date2);
     }
 }
