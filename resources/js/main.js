cfecfd7a1b69c8a0402a3bac1fcdd6da9dad57af
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-11-19 15:09:35 +0100

    Korrektur Syntax

--- resources/js/main.js
+++ resources/js/main.js
@@ -67,8 +67,8 @@ function FerienZeit(d1, d2){
     this.d1= d1;
     this.d2= d2;
     this.checkIfWithin=(datestr)=>{
-        let date1=new Date(d1.split(".")[2], (d1.split(".")[1])-1,d1.split("."[0]));
-        let date2=new Date(d2.split(".")[2], (d2.split(".")[1])-1, d2.split("."[0]));
+        let date1=new Date(d1.split(".")[2], (d1.split(".")[1])-1,d1.split(".")[0]);
+        let date2=new Date(d2.split(".")[2], (d2.split(".")[1])-1, d2.split(".")[0]);
         let compDate= new Date(datestr.split(".")[2], datestr.split(".")[1]-1, datestr.split("."[0]));
         console.log(datestr.split("."));
         console.log(d1.split("."));
@@ -153,9 +153,9 @@ function fillDaySlots(){//Desktop version of web page, several days on one page
                 if(fz.checkIfWithin(compareDateString)){
                     available = false;
                 }
-                let date1=new Date(fz.d1.split(".")[2], (fz.d1.split(".")[1])-1,fz.d1.split("."[0]));
-                let date2=new Date(fz.d2.split(".")[2], (fz.d2.split(".")[1])-1, fz.d2.split("."[0]));
-                let compDate= new Date(compareDateString.split(".")[2], compareDateString.split(".")[1]-1, compareDateString.split("."[0]));
+                let date1=new Date(fz.d1.split(".")[2], (fz.d1.split(".")[1])-1,fz.d1.split(".")[0]);
+                let date2=new Date(fz.d2.split(".")[2], (fz.d2.split(".")[1])-1, fz.d2.split(".")[0]);
+                let compDate= new Date(compareDateString.split(".")[2], compareDateString.split(".")[1]-1, compareDateString.split(".")[0],0,0);
                 console.log(date1);
                 console.log(date2);
                 console.log(compDate);
