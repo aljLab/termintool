e2988ae26b175c6c89a8e4a0cdc77670b5c73386
Aljoscha Labonte <aljoschalabonte@rocketmail.com>
2023-11-19 15:11:07 +0100

    Korrektur Syntax

--- resources/js/main.js
+++ resources/js/main.js
@@ -70,13 +70,7 @@ function FerienZeit(d1, d2){
         let date1=new Date(d1.split(".")[2], (d1.split(".")[1])-1,d1.split(".")[0]);
         let date2=new Date(d2.split(".")[2], (d2.split(".")[1])-1, d2.split(".")[0]);
         let compDate= new Date(datestr.split(".")[2], datestr.split(".")[1]-1, datestr.split("."[0]));
-        console.log(datestr.split("."));
-        console.log(d1.split("."));
-        console.log(d2.split("."));
-        console.log(date1);
-        console.log(date2);
-        console.log(compDate);
-        return (this.date1 <= compDate)&&( compDate<=this.date2);
+        return (date1 <= compDate)&&(compDate<=date2);
     }
 }
 /*------------------------Index Page handling------------------------------*/
@@ -153,12 +147,6 @@ function fillDaySlots(){//Desktop version of web page, several days on one page
                 if(fz.checkIfWithin(compareDateString)){
                     available = false;
                 }
-                let date1=new Date(fz.d1.split(".")[2], (fz.d1.split(".")[1])-1,fz.d1.split(".")[0]);
-                let date2=new Date(fz.d2.split(".")[2], (fz.d2.split(".")[1])-1, fz.d2.split(".")[0]);
-                let compDate= new Date(compareDateString.split(".")[2], compareDateString.split(".")[1]-1, compareDateString.split(".")[0],0,0);
-                console.log(date1);
-                console.log(date2);
-                console.log(compDate);
             });
             if(available){
                 let div = document.getElementById(`${bhweekdays[l]}`);
